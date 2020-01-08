a4b68b33d811af6ea5c9ef2c0111ec66dfd234f6
Allie Robinson <allierob93@gmail.com>
2020-01-07 22:37:46 -0600

    EditUser has a working GET and PUT request

--- storefront-app/src/components/EditUser.js
+++ storefront-app/src/components/EditUser.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axiosWithAuth from '../utils/axiosWithAuth';
 
-const EditUser = () => {
+const EditUser = (props) => {
+
+    console.log("props", props)
 
     const [user, setUser] = useState({
         id: '',
@@ -12,9 +14,14 @@ const EditUser = () => {
 
     const getUser = () => {
         axiosWithAuth()
-            .get(`/users/${user.id}`)
+            .get(`/users/${props.match.params.id}`)
             .then(res => setUser(res.data))
     }
+
+    useEffect(() => {
+        getUser()
+    }, [])
+
 
     const changeHandler = (e) => {
         setUser({
@@ -23,12 +30,19 @@ const EditUser = () => {
         })
     }
 
+    const submitForm = (e) => {
+        e.preventDefault();
+        axiosWithAuth()
+            .put(`/users/${props.match.params.id}`, user)
+            .then(console.log("submitForm fired", user))
+    }
+
     console.log("user", user)
 
     return (<>
         <h2>Edit Username</h2>
-
-        <form>
+        <p>Current username: {user.username}</p>
+        <form onSubmit={submitForm}>
             <input
                 placeholder="Username"
                 name="username"
