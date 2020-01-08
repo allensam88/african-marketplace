e3bf7e1113838e1641250ba7ebf340dd25032ea7
blakelower <blakelower1999@gmail.com>
2020-01-08 19:13:15 +0700

    created a post req

--- storefront-app/src/components/postItems.js
+++ storefront-app/src/components/postItems.js
@@ -3,49 +3,72 @@ import axiosWithAuth from '../utils/axiosWithAuth';
 
 const Login = props => {
   const [user, setUser] = useState({
-    username: " ",
-    password: " "
+    name: " ",
+    descipition: " ",
+    price: " ",
+    category: " ",
+    location: " "
   });
 
   const handleChange = event => {
-    const { value, name } = event.target;
     setUser({
       ...user,
-      [name]: value
+      [event.target.name]: event.target.value,
     });
   };
 
   const handleSubmit = event => {
     event.preventDefault();
     axiosWithAuth()
-      .post('https://african-marketplace-1.herokuapp.com/api/login', user)
+      .post('https://african-marketplace-1.herokuapp.com/api/items', user)
       .then(response => {
-        localStorage.setItem("token", response.data.payload);
-        props.history.push("/putitems");
+          localStorage.setItem('token', response.data.token)
+          props.history.push(`/MarketPlace/${response.data.id}`)
       })
-      .catch(error => console.log("Failed Login", error));
-        localStorage.setItem("token",);
-        props.history.push("/putitems");
-  };
-
+      .catch(err => console.log(err))
+}
   return (
     <div>
-      <h1 className="header">Welcome to the African Market App!</h1>
+      <h1>Welcome to the African Market App!</h1>
       <form onSubmit={handleSubmit}>
-        <label>Username:</label>
+        <label>Name</label>
         <input
           type="text"
-          name="username"
-          placeholder="Username"
-          value={user.username}
+          name="Name"
+          placeholder="Name"
+          value={user.name}
           onChange={handleChange}
         />
-        <label>Password:</label>
+        <label>Descripition</label>
         <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={user.password}
+          type="text"
+          name="description"
+          placeholder="Description"
+          value={user.description}
+          onChange={handleChange}
+        />
+         <label>Price</label>
+        <input
+          type="text"
+          name="price"
+          placeholder="Price"
+          value={user.price}
+          onChange={handleChange}
+        />
+        <label>Category</label>
+        <input
+          type="text"
+          name="category"
+          placeholder="Category"
+          value={user.category}
+          onChange={handleChange}
+        />
+         <label>Location</label>
+        <input
+          type="text"
+          name="location"
+          placeholder="Location"
+          value={user.location}
           onChange={handleChange}
         />
         <button type="submit">Log In</button>
