78a6932a1837a5cace6d3f560d85dd9f7baaff20
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-05-16 12:40:34 -0700

    changed server base url

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const axiosWithAuth = () => {
-  const token = localStorage.getItem('token');
-  return axios.create({
-    baseURL: 'https://african-marketplace-1.herokuapp.com/api',
-    headers: {
-    'Content-Type': 'application/json',
-     'authorization': `${token}`
-    }
-  })
+	const token = localStorage.getItem('token');
+	return axios.create({
+		baseURL: 'https://african-marketplace-server.herokuapp.com/',
+		headers: {
+			'Content-Type': 'application/json',
+			'authorization': `${token}`
+		}
+	})
 }
 
 export default axiosWithAuth;
